509ec630fb47b826edc10282b79a722139d22c6d
Omnimus34 <salvadorjavier360@gmail.com>
2025-05-13 15:26:30 -0600

    se hizo un cambio
    se hizo un cambio en parte de planilla

--- src/modelos/Planilla.java
+++ src/modelos/Planilla.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Planilla {
-    private List<Empleado> empleados;
+    private final List<Empleado> empleados;
 
     public Planilla() {
         this.empleados = new ArrayList<>();
